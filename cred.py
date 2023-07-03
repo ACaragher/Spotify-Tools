4e29b5a8182a9103f05a925985ca2332ef69c0ea
Adam Caragher <caragher.adam@gmail.com>
2023-07-03 17:33:19 +0100

    Intial commit, basic functionality implemented

---
+++ cred.py
@@ -0,0 +1,6 @@
+# Enter values from https://developer.spotify.com/dashboard into respective fields below
+# Used to retreive token from spotify authorization
+
+client_ID=''
+client_SECRET=''
+redirect_url=''
