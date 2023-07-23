718366e9ea5acbf38161bd9e5517e8609ae327e1
Adam Caragher <caragher.adam@gmail.com>
2023-07-23 16:40:35 +0100

    Update cred.py

--- spotify_tools/spotify_tools/cred.py
+++ spotify_tools/spotify_tools/cred.py
@@ -1,4 +1,4 @@
-client_ID='0c5dc9ea1d5141799dd8d93e0ab7d81f'
-client_SECRET='f6638c49ec604b84ad6ca367f6d95807'
+client_ID=''
+client_SECRET=''
 redirect_uri='http://localhost:8000/social/complete/spotify/'
 scope='app-remote-control streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state user-read-currently-playing user-library-modify user-library-read playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public'
