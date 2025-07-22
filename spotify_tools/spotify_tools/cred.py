fcda5dd60c018241dd238c893c76f18c9e316d4d
Adam Caragher <115255350+ACaragher@users.noreply.github.com>
2025-07-22 01:38:56 +0100

    Update app to run on Azure Web Service
    Update settings.py and create master_spotifytools.yml in order for app to run on Azure Web Service through GitHub Actions

--- spotify_tools/spotify_tools/cred.py
+++ spotify_tools/spotify_tools/cred.py
@@ -1,4 +1,6 @@
-client_ID=''
-client_SECRET=''
-redirect_uri='http://localhost:8000/social/complete/spotify/'
+import os
+
+client_ID=os.getenv('SPOTIFY_CLIENT_ID')
+client_SECRET=os.getenv('SPOTIFY_API_KEY')
+redirect_uri='https://www.caragher.ie/social/complete/spotify/'
 scope='app-remote-control streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state user-read-currently-playing user-library-modify user-library-read playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public'
--- spotify_tools/spotify_tools/settings.py
+++ spotify_tools/spotify_tools/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from . import cred
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=vzqd%r@kh4t%3*)rt%vi40#l&4jqcdxkv7l2d65du=05ttd%t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.caragher.ie',
+                 'caragher.ie',
+                 'spotifytools-gqgxdsg8asfvemh9.swedencentral-01.azurewebsites.net',
+                 '169.254.130.2' ]
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 SOCIAL_AUTH_SPOTIFY_KEY = cred.client_ID
@@ -72,6 +76,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ADD THIS HERE
 ]
 
 ROOT_URLCONF = 'spotify_tools.urls'
@@ -143,11 +148,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 LOGIN_REDIRECT_URL = 'home:login'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
