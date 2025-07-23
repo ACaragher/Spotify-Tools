8b55ddd90e91d205ae80136f81e98fa63ce875ef
Adam Caragher <115255350+ACaragher@users.noreply.github.com>
2025-07-23 12:22:44 +0100

    Update settings.py

--- spotify_tools/spotify_tools/settings.py
+++ spotify_tools/spotify_tools/settings.py
@@ -153,4 +153,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home:login'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
